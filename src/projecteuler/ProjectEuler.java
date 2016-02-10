41549ab02c4d3e86a3f67246136c2aae9615d0d9
rudolf-jvt <rudolf@jongensvantechniek.nl>
2016-02-10 11:42:06 +0100

    INIT

---
+++ src/projecteuler/ProjectEuler.java
@@ -0,0 +1,19 @@
+/*
+ * See https://projecteuler.net/archives for the ProjectEuler challenges!
+ */
+package projecteuler;
+
+/**
+ *
+ * @author Rudolf Sneep
+ */
+public class ProjectEuler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
